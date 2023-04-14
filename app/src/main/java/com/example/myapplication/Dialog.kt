d359777a872fd9ff2c501790b9754ee6b5e53f4f
Mustafo <mustafa.mahmudov@payme.uz>
2023-04-14 14:12:21 +0500

    Init file

---
+++ app/src/main/java/com/example/myapplication/Dialog.kt
@@ -0,0 +1,65 @@
+package com.example.myapplication
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+
+@Composable
+fun AlertDialogSample() {
+    MaterialTheme {
+        Column {
+            val openDialog = remember { mutableStateOf(false)  }
+
+            Button(onClick = {
+                openDialog.value = true
+            }) {
+                Text("Click me")
+            }
+
+            if (openDialog.value) {
+
+                AlertDialog(
+                    onDismissRequest = {
+                        // Dismiss the dialog when the user clicks outside the dialog or on the back
+                        // button. If you want to disable that functionality, simply use an empty
+                        // onCloseRequest.
+                        openDialog.value = false
+                    },
+                    title = {
+                        Text(text = "Dialog Title")
+                    },
+                    text = {
+                        Text("Here is a text ")
+                    },
+                    confirmButton = {
+                        Button(
+
+                            onClick = {
+                                openDialog.value = false
+                            }) {
+                            Text("This is the Confirm Button")
+                        }
+                    },
+                    dismissButton = {
+                        Button(
+
+                            onClick = {
+                                openDialog.value = false
+                            }) {
+                            Text("This is the dismiss Button")
+                        }
+                    }
+                )
+            }
+        }
+
+    }
+}
